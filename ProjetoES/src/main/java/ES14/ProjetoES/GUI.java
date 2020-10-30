3ce0ecdee63c556381576d77e6727e1408bfeb5c
fnpms-iscte <fnpms@iscte-iul.pt>
2020-10-30 00:49:37 +0000

    Ler Excel

--- ProjetoES/src/main/java/ES14/ProjetoES/GUI.java
+++ ProjetoES/src/main/java/ES14/ProjetoES/GUI.java
@@ -4,12 +4,19 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Vector;
 
 import javax.swing.*;
 import javax.swing.event.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.DataFormatter;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -19,6 +26,8 @@ public class GUI {
 
 	private JFrame janela;
 	private XSSFSheet sheet;
+	private String[] headers;
+	private String[][] data;
 
 	public GUI() {
 		janela = new JFrame("DetetorDefeitos3000");
@@ -47,7 +56,8 @@ public class GUI {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 
-				JFileChooser chooser = new JFileChooser(".");
+				JFileChooser chooser = new JFileChooser(
+						"C:\\Users\\fnpm\\OneDrive\\Ambiente de Trabalho\\ISCTE\\3ºAno\\1º Semestre\\ES\\Defeitos.xlsx");
 				chooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
 
 				int returnValue = chooser.showOpenDialog(null);
@@ -67,7 +77,9 @@ public class GUI {
 					janelaExcel.setSize(1200, 900);
 					janelaExcel.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-					JTable tabela = new JTable();
+					TableModel table = new DefaultTableModel(data, headers);
+
+					JTable tabela = new JTable(table);
 					JScrollPane center = new JScrollPane(tabela);
 
 					janelaExcel.add(center);
@@ -101,16 +113,23 @@ public class GUI {
 		new GUI().open();
 	}
 
-	/*
-	 * public void setUpGui() { JFrame janelaExcel = new
-	 * JFrame(selectedFile.getName()); janela.pack(); janela.setSize(1200, 900);
-	 * janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE); }
-	 */
-
 	public void readExcel(String path) throws Exception {
 		XSSFWorkbook workbook = new XSSFWorkbook(new File(path));
 		sheet = workbook.getSheetAt(0);
 
-	}
+		DataFormatter dataFormatter = new DataFormatter();
+		headers = new String[12];
+		data = new String[sheet.getLastRowNum()][12];
 
+		for (Row row : sheet) {
+			for (Cell cell : row) {
+				String cellValue = dataFormatter.formatCellValue(cell);
+				if (cell.getRowIndex() != 0) {
+					data[row.getRowNum() - 1][cell.getColumnIndex()] = cellValue;
+				} else {
+					headers[cell.getColumnIndex()] = cellValue;
+				}
+			}
+		}
+	}
 }
