5dd91e0ce70818293640ef25a7399f4eedbeb32f
fnpms-iscte <fnpms@iscte-iul.pt>
2020-11-18 16:26:53 +0000

    Entrega Intercalar - done

--- ProjetoES/src/main/java/ES14/ProjetoES/GUI.java
+++ ProjetoES/src/main/java/ES14/ProjetoES/GUI.java
@@ -5,8 +5,10 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
 import javax.swing.table.DefaultTableModel;
 import javax.swing.table.TableModel;
 
@@ -26,10 +28,7 @@ public class GUI {
 	private TableModel table;
 	private JPanel painelAux;
 
-	/*
-	 * Boolean para verificar que não é a primeira vez a adicionar o painel dos
-	 * metodos
-	 */
+	/* verifica que não é a primeira vez a adicionar o painel dos metodos */
 	private boolean aux = false;
 
 	public GUI() {
@@ -37,7 +36,6 @@ public class GUI {
 		janela.pack();
 		janela.setSize(1200, 900);
 		janela.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-
 		addFrameContent();
 
 	}
@@ -103,7 +101,8 @@ public class GUI {
 					ferramentas.remove(painelAux);
 				}
 
-				String[] a = { "MethodID" };
+				String[] header = { "MethodID" };
+				String[] headerIndicadores = { "Indicador", "Valor" };
 				String ferramentaSelecionada = (String) flags2.getSelectedItem(); // falta o caso de "regra a definir"
 
 				Algoritmo alg = new Algoritmo(sheet, ferramentaSelecionada);
@@ -111,12 +110,21 @@ public class GUI {
 				painelAux = new JPanel(new GridLayout(1, 2));
 				JPanel painelMetodos = new JPanel(new FlowLayout()); // experimentar com gridLayout
 				JPanel painelResultados = new JPanel(new FlowLayout());
-				JTable tabelaMethodID = new JTable(arrayToMatrix(alg.getMethods()), a);
+
+				JTable tabelaMethodID = new JTable(arrayToMatrix(alg.getMethods()), header);
+				JTable tabelaIndicadores = new JTable(mapToMatrix(alg.getIndicadores()), headerIndicadores);
+
 				JScrollPane scroll = new JScrollPane(tabelaMethodID);
+				JScrollPane scroll1 = new JScrollPane(tabelaIndicadores);
+
+				painelResultados.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+						"Indicadores de Qualidade", TitledBorder.CENTER, TitledBorder.TOP));
+				painelMetodos.setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(),
+						"Métodos com Erros", TitledBorder.CENTER, TitledBorder.TOP));
 
 				painelMetodos.add(scroll);
+				painelResultados.add(scroll1);
 
-				painelAux.removeAll();
 				painelAux.add(painelMetodos);
 				painelAux.add(painelResultados);
 				ferramentas.add(painelAux, BorderLayout.CENTER);
@@ -173,6 +181,21 @@ public class GUI {
 
 		for (Integer i : aux) {
 			aux1[l][0] = i.toString();
+			l++;
+		}
+		return aux1;
+	}
+
+	private String[][] mapToMatrix(HashMap<String, Integer> aux) {
+		int c = 0;
+		int l = 0;
+		String[][] aux1 = new String[aux.size()][2];
+
+		for (String i : aux.keySet()) {
+			c = 0;
+			aux1[l][c] = i;
+			c++;
+			aux1[l][c] = aux.get(i).toString();
 			l++;
 		}
 		return aux1;
