a95f066248fc291c4abde9ba0ea47d8cd0e8dbc0
tmmss1-iscte <tmmss1@iscte-iul.pt>
2020-12-11 12:54:42 +0000

    Classes JUnit do ExcelControlller e suite AllTests criadas

---
+++ ProjetoES/src/test/java/ES14/ProjetoES/AllTests.java
@@ -0,0 +1,18 @@
+package ES14.ProjetoES;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+public class AllTests {
+
+	public static Test suite() {
+		TestSuite suite = new TestSuite(AllTests.class.getName());
+
+		suite.addTestSuite(AlgoritmoTest.class);
+		suite.addTest(AppTest.suite());
+		suite.addTestSuite(ExcelControllerTest.class);
+		
+		return suite;
+	}
+
+}
---
+++ ProjetoES/src/test/java/ES14/ProjetoES/ExcelControllerTest.java
@@ -0,0 +1,20 @@
+package ES14.ProjetoES;
+
+
+import org.junit.jupiter.api.Test;
+
+import junit.framework.TestCase;
+
+public class ExcelControllerTest extends TestCase{
+
+	public ExcelControllerTest() {
+		super();
+	}
+	
+	
+	@Test
+	public void testReadExcel() {
+		//Verificar se está a ler corretamente posições aleatórias do ficheiro excel
+	}
+
+}
